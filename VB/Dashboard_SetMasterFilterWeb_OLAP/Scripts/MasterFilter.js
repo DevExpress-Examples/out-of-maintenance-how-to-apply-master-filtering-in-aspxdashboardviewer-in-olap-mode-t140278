dba81747826150528cfd0e875425f8d95ab97a5a
Andrey Kuleshov <kaa-tula@ya.ru>
2019-04-25 11:27:54 +0300

    Add info about removing old Web Viewer to readme.md

--- VB/Dashboard_SetMasterFilterWeb_OLAP/Scripts/MasterFilter.js
+++
@@ -1,5 +0,0 @@
-﻿function onButtonClick(s, e) {
-    var cardValues1 = ['[Date].[Calendar Year].&[2001]', '[Date].[Calendar Quarter of Year].&[CY Q4]'];
-    var cardValues2 = ['[Date].[Calendar Year].&[2002]', '[Date].[Calendar Quarter of Year].&[CY Q1]'];
-    WebViewer.SetMasterFilter("cardDashboardItem1", [cardValues1, cardValues2]);
-}
